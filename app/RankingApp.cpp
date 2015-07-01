6c748e13e874620b613cc8ecdce27ad8c8a873ec
Volker Knollmann <v.knollmann@web.de>
2015-07-01 18:19:40 +0200

    Added a first, primitive navigation bar to the web application.

--- app/RankingApp.cpp
+++ app/RankingApp.cpp
@@ -1,12 +1,49 @@
 
 #include <Wt/WText>
 #include <Wt/WContainerWidget>
+#include <Wt/WNavigationBar>
+#include <Wt/WMenu>
+#include <Wt/WBootstrapTheme>
+#include <Wt/WAnchor>
 
 #include "RankingApp.h"
 
 RankingApp::RankingApp::RankingApp(const Wt::WEnvironment& env)
   :WApplication(env)
 {
+  WBootstrapTheme* theme = new WBootstrapTheme();
+  theme->setVersion(WBootstrapTheme::Version3);
+  setTheme(theme);
   setTitle("Hello World");
-  root()->addWidget(new WText("Hey there!"));
+  createMenuBar();
+
+
+}
+
+//----------------------------------------------------------------------------
+
+void RankingApp::RankingApp::createMenuBar()
+{
+  WContainerWidget* c = new WContainerWidget();
+
+  // the navigation bar itself
+  WNavigationBar* navBar = new WNavigationBar(c);
+  navBar->setTitle("USC Badminton", "http://www.google.com");
+  navBar->setResponsive(true);
+
+  // the container for the main content of the screen;
+  // the content is switched by different menu selections
+  mainContainer = new WStackedWidget(c);
+  mainContainer->setStyleClass("contents");
+
+  // the menu on the left
+  WMenu* leftMenu = new WMenu(mainContainer, c);
+  navBar->addMenu(leftMenu);
+  leftMenu->setInternalPathEnabled("/basePath");
+  leftMenu->addItem("Einzel", new WText("Einzel-RL"));
+  leftMenu->addItem("Doppel", new WText("Doppel-RL"));
+
+  c->addWidget(mainContainer);
+
+  root()->addWidget(c);
 }
--- app/RankingApp.h
+++ app/RankingApp.h
@@ -1,12 +1,17 @@
 #ifndef RANKINGAPP_H
 #define RANKINGAPP_H
 
+#include <memory>
+
 #include <Wt/WApplication>
+#include <Wt/WContainerWidget>
+#include <Wt/WStackedWidget>
 
 #include "SqliteDatabase.h"
 
 using namespace Wt;
 using namespace SqliteOverlay;
+using namespace std;
 
 namespace RankingApp {
 
@@ -14,6 +19,11 @@ namespace RankingApp {
   {
   public:
     RankingApp(const WEnvironment& env);
+
+  private:
+    void createMenuBar();
+
+    WStackedWidget* mainContainer;
   };
 
 }
